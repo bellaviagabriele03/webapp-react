035019f05219b84f9185d2aa197db61bbe33eb03
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-01-20 16:14:00 +0100

    feat: layout, pages and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MoviesLayout from "./Layouts/MoviesLayout";
+import Home from "./Pages/Home";
+import Movies from "./Pages/Movies";
+import About from "./Pages/About";
 
 function App() {
-  
+
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MoviesLayout/>}>
+            <Route element={<Home/>} path="/" />
+            <Route element={<Movies/>} path="/movies" />
+            <Route element={<About/>} path="/about" />
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Pages/Movies.jsx
@@ -0,0 +1,7 @@
+export default function Movies() {
+    return (
+        <>
+            <h1>Pagina Film</h1>
+        </>
+    )
+}
