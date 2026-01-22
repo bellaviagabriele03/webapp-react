778d35ca756ddb2249240833849e342fbec89b88
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-01-22 18:12:54 +0100

    feat: cambiata disposizione grafica

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -8,10 +8,10 @@ export default function Details() {
     const [show, setShow] = useState([]);
     const [reviews, setReviews] = useState([])
     useEffect(() => {
-        axios.get(`http://localhost:3000/api/movies/${id}`).then((resp) => { 
+        axios.get(`http://localhost:3000/api/movies/${id}`).then((resp) => {
             console.log(resp.data.result);
             console.log(resp.data.reviews);
-            
+
             setShow(resp.data.result)
             setReviews(resp.data.reviews)
         })
@@ -23,28 +23,24 @@ export default function Details() {
 
     return (
         <>
-            <h1 className="text-center">Pagina Dettagli film: {show.title}</h1>
+            <h1 className="text-center bg-black mb-0 text-white">{show.title}</h1>
 
             <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-2">
-                        <div className="card w-100 h-100">
-                            <div className="card-body">
-                                <img className="card-img-top" src={urlImg + show.image} alt="" />
-                                {reviews.map((rev)=>{
-                                    return (
-                                        <>
-                                            <p className="card-text"><span className="text-success">user:</span> {rev.name} <span className="text-warning">Vote:</span> {rev.vote}</p>
-                                        </>
-                                    )
-                                })}
-                            </div>
-                        </div>
+                <div className="row">
+                    <div className="col-4 p-3 bg-black">
+                        <img className="w-100 h-100" src={urlImg + show.image} alt="" />
+                    </div>
+                    <div className="col-8 text-center bg-black text-white d-flex justify-content-center align-items-center flex-column">
+                        <h3>Autore: {show.director}</h3>
+                        <p>{show.abstract}</p>
+                        <p>Genere: {show.genre}</p>
+                        <span>Uscito nel {show.release_year}</span>
                     </div>
                 </div>
             </div>
+            <h1 className="text-center bg-black py-4"></h1>
 
-
+            
         </>
     )
 
